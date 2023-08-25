e385a9e844a96e90220d327cf5887d980cb693b1
Kyungho0511 <kleearchitecture@gmail.com>
2023-08-26 07:22:33 +0900

    React CSS libraries added

---
+++ css/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Button1 from './components/Button1';
+import Button2 from './components/Button2';
+import StyledComponents from './StyledComponents';
+import TailwindComponent from './TailwindComponent';
+
+export default function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+      <StyledComponents />
+      <TailwindComponent />
+    </>
+  );
+}
---
+++ css/src/components/Button1.jsx
@@ -0,0 +1,7 @@
+import styles from './Button1.module.css'
+
+export default function Button1() {
+  return (
+    <button className={styles.button}>Button1</button>
+  );
+}
