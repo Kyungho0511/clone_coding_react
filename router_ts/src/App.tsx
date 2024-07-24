b77864befd99acc3e1fa0518081299286ab53603
kyungho0511 <kleearchitecture@gmail.com>
2024-07-23 20:36:06 -0400

    React_ts router added

---
+++ router_ts/src/App.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import NotFound from './pages/NotFound';
+import Videos from './pages/Videos';
+import Root from './pages/Root';
+import Home from './pages/Home';
+import VideoDetail from './pages/VideoDetail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/videos', element: <Videos /> },
+      { path: '/videos/:videoId', element: <VideoDetail /> },
+    ]
+  }
+]);
+
+export default function App() {
+  return (
+  <RouterProvider router={router} />
+  );
+}
